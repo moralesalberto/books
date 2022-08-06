b027ba92a8d578782c60dd387ee1177d49c6a039
moralesalberto <alberto@moralitos.com>
2022-08-05 23:35:07 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book } from './book';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+	providers: [HttpClient]
 })
 export class AppComponent {
-  title = 'home-library';
+  title = 'books';
+  books: Book[];
+
+  constructor(private http: HttpClient) {
+    http.get('http://localhost:3000/books.json')
+      .subscribe(res: Book[] => this.books = res);
+  }
 }
